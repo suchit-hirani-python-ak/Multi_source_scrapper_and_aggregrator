7de1e3c99d805ce722aea820ef48ccc3df2329c7
suchit-hirani-python-ak <suchit.hirani@armakuni.com>
2026-04-13 19:20:00 +0530

    feat: AT3-T5311 database_setup

---
+++ app/core/config.py
@@ -0,0 +1,21 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    algorithm: str
+    mongo_url: str
+    redis_url: str
+    access_token: SecretStr
+    refresh_token: SecretStr
+    access_expire_in_minutes: int 
+    refresh_expire_in_days: int 
+    encription: str
+    admin_name: SecretStr
+    admin_pass: SecretStr
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+        
+settings = Settings() # type: ignore
